3862d004227ca07aba331041d6fc0eec7b11464e
Brian Chatterton <brian.chatterton@gmail.com>
2023-08-26 22:57:02 +0100

    pnpm lock file update

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,15 +6,6 @@ declare global {
 		interface Locals {
 			wss?: ExtendedWebSocketServer;
 		}
-		interface Platform {
-		    env: {
-		        COUNTER: DurableObjectNamespace;
-		    };
-		    context: {
-		        waitUntil(promise: Promise<any>): void;
-	       	};
-	       	caches: CacheStorage & { default: Cache }
-		}
 		// interface PageData {}
 		// interface Platform {}
 	}
